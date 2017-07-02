bc1aa88963a6be9366b3b5f2bcc2e37effb8a95c
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 00:29:22 +0900

    Add action creators for login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILURE,
+  LOGIN_USER,
 } from './types';
 
 export const emailChanged = (text) => {
@@ -15,4 +19,36 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text,
   };
+};
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    dispatch({
+      type: LOGIN_USER,
+    });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFailure(dispatch));
+      });
+  };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user,
+  });
+};
+
+const loginUserFailure = (dispatch) => {
+  console.log(LOGIN_USER_FAILURE);
+  dispatch({
+    type: LOGIN_USER_FAILURE,
+  });
 };
