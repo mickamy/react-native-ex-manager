59f7f3f434b1cd827aaed337c2a1d4418cec459f
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 02:15:43 +0900

    Create dummy scene

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
-    <Router>
+    <Router sceneStyle={{ paddingTop: 60 }}>
       <Scene
         key={'login'}
         component={LoginForm}
         title='Login'
+      />
+      <Scene
+        key={'employeeList'}
+        component={EmployeeList}
+        title='Employees'
       />
     </Router>
   );
