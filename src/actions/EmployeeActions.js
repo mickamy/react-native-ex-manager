c84416b0e01f6fea3d505264fc044dd7aa84ef05
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 03:05:48 +0900

    Create actions for add new employee

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,10 @@
+import {
+  UPDATE_EMPLOYEE,
+} from './types';
+
+export const updateEmployee = ({ prop, value }) => {
+  return {
+    type: UPDATE_EMPLOYEE,
+    payload: { prop, value },
+  };
+};
--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER = 'login_user';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAILURE = 'login_user_failuer';
+
+export const UPDATE_EMPLOYEE = 'update_emplyoee';
