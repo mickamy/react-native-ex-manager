395e7c5311b00ef961fce0f3cdcc4a2c8511bad5
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 03:06:06 +0900

    Create reducer to handle employee update

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,22 @@
+import {
+  UPDATE_EMPLOYEE,
+} from '../actions';
+
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: '',
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UPDATE_EMPLOYEE: {
+      const { prop, value } = action.payload;
+      return { ...state,
+        [prop]: value
+      };
+    }
+    default:
+      return state;
+  }
+};
