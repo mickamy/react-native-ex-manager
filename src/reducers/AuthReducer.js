7f858f8926a7430044ecfddbd3a2b1ba22633b4d
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-02 22:44:44 +0900

    Make import statement multiline

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,4 +1,7 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+} from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
