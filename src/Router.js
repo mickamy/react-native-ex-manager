33dd2ad0678d07d85a730e7dcb16fdca8c6ef11a
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 02:30:49 +0900

    Create nesting scenes

--- src/Router.js
+++ src/Router.js
@@ -6,16 +6,20 @@ import EmployeeList from './components/EmployeeList';
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 60 }}>
-      <Scene
-        key={'login'}
-        component={LoginForm}
-        title='Login'
-      />
-      <Scene
-        key={'employeeList'}
-        component={EmployeeList}
-        title='Employees'
-      />
+      <Scene key={'auth'}>
+        <Scene
+          key={'login'}
+          component={LoginForm}
+          title='Login'
+        />
+      </Scene>
+      <Scene key={'main'}>
+        <Scene
+          key={'employeeList'}
+          component={EmployeeList}
+          title='Employees'
+        />
+      </Scene>
     </Router>
   );
 };
