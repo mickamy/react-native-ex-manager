db9ef6c749adb15be8aa835a403db3f502bb2c6a
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 01:24:31 +0900

    Create Login scene

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import Config from 'react-native-config';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
@@ -28,7 +28,7 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
     </Provider>
     );
   }
