aa3792d0e6b942575927668c69c4d7f1da7c10b9
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 03:57:41 +0900

    Fix typo `emplyoee` ~> `employee`

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,4 +4,4 @@ export const LOGIN_USER = 'login_user';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAILURE = 'login_user_failuer';
 
-export const UPDATE_EMPLOYEE = 'update_emplyoee';
+export const UPDATE_EMPLOYEE = 'update_employee';
