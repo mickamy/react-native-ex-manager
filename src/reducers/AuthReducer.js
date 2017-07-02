cf7c67db217e5706f1a7c276bdccf94b6d55452c
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-03 00:29:10 +0900

    Handle login in reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,19 +1,42 @@
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILURE,
+  LOGIN_USER,
 } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
   password: '',
+  loading: false,
+  user: null,
+  error: '',
 };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.palyoad };
     case PASSWORD_CHANGED:
-      return { ...state, password: action.palyoad };
+      return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state,
+        loading: true,
+        error: ''
+      };
+    case LOGIN_USER_SUCCESS:
+      return { ...state,
+        ...INITIAL_STATE,
+        user: action.payload,
+      };
+    case LOGIN_USER_FAILURE:
+      return { ...state,
+        password: '',
+        loading: false,
+        error: 'Autnetication Failed.',
+      };
     default:
       return state;
   }
