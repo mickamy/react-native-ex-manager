2c9621c1b70bbe4c0cfd4ecb9bc36127e3392e5a
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-02 10:26:05 +0900

    Initialize firebase on mount

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import Config from 'react-native-config';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: Config.API_KEY,
+      authDomain: 'react-native-ex-manager.firebaseapp.com',
+      databaseURL: 'https://react-native-ex-manager.firebaseio.com',
+      projectId: 'react-native-ex-manager',
+      storageBucket: 'react-native-ex-manager.appspot.com',
+      messagingSenderId: Config.MESSAGEING_SENDER_ID
+    });
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
